b2082d182cd8436474d68e759a207cc36f60d517
Tabita-Marcut <tabitamarcut@yahoo.com>
2025-11-12 14:37:59 +0100

    fitbit_secrets.dart

---
+++ lib/fitbit_secrets.dart
@@ -0,0 +1,6 @@
+class FitbitSecrets {
+  static const clientId = 'DEINE_CLIENT_ID';
+  static const clientSecret = 'DEIN_CLIENT_SECRET';
+  static const redirectUri = 'sleepbalance://fitbit-auth';
+  static const callbackScheme = 'sleepbalance';
+}
