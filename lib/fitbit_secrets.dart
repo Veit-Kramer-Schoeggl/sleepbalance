f6dd265ed0500c8650fd35711086e90af4da10ac
Veit <veit.kramer@edu.fh-joanneum.at>
2025-11-16 13:19:33 +0100

    security: Remove Fitbit credentials and test code from version control

--- lib/fitbit_secrets.dart
+++
@@ -1,6 +0,0 @@
-class FitbitSecrets {
-  static const clientId = '23TPMQ';
-  static const clientSecret = '45c3f40db0737b8199dbd3ce58d176a6';
-  static const redirectUri = 'sleepbalance://fitbit-auth';
-  static const callbackScheme = 'sleepbalance';
-}
